d5ea2d744d6a0c1098228699ce9185005e4a8993
Student <Student@L411-S27>
2018-09-28 12:38:10 +0800

    Added a new line of code at main method.

--- src/gitpush/GitPush.java
+++ src/gitpush/GitPush.java
@@ -1,9 +1,25 @@
 package gitpush;
+
 public class GitPush {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        
-        
+
+    /**
+     *
+     * @param base
+     * @param exponent
+     * @return
+     */
+    public static double power(double base, int exponent) {
+        double total = 1;
+        for (int counter = 1; counter <= exponent; counter++) {
+            total*= base;
+        }
+       System.out.println(total);
+                return total;
     }
-    
+
+    public static void main(String[] args) {
+        power(2,3);
+
+    }
+
 }
